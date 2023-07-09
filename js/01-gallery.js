16b5d51d5a0dfa05ccfe0ca6674e9e1200d5c88a
hitmen798 <124578265+hitmen798@users.noreply.github.com>
2023-07-09 14:04:42 +0200

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+gallery.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  
+  const imageSource = event.target.dataset.source;
+  
+  const instance = basicLightbox.create(`
+    <img src="${imageSource}" width="800" height="600">
+  `);
+  
+  instance.show();
+}
